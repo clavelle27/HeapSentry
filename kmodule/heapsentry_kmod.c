edcd8f5445bf18ee6363c3e26e380f5696ca8d8b
vishalnayak <vishalnayakv@gmail.com>
2015-09-15 15:14:27 -0400

    Empty kernel module

---
+++ kmodule/heapsentry_kmod.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL v2");
+
+static int __init mod_entry_func(void)
+{
+	return 0;
+}
+
+static void __exit mod_exit_func(void)
+{
+}
+
+module_init(mod_entry_func);
+module_exit(mod_exit_func);
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("vnayak@cs.stonybrook.edu");
+MODULE_DESCRIPTION("HeapSentry Kernel Module");
