60d623243b83e61060d3bb4ed018b0aecb919a69
vishalnayak <vishalnayakv@gmail.com>
2015-09-22 03:15:17 -0400

    Removed libheapsentryu information from binary. Using it now as LD_PRELOAD entry

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
+
+# LD_PRELOAD lists libraries that are to be mapped into the
+# binary's process address space before execution.
+# This makes libheapsentryu's malloc() and free() to be found
+# before the ones belonging to libc.
+export LD_PRELOAD=./lib/libheapsentryu.so
 ./libtest
