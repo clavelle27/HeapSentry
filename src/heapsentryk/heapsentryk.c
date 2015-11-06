2696a78002dd1a0061052d5c12d75482da9cadae
vishalnayak <vishalnayakv@gmail.com>
2015-11-05 20:01:28 -0500

    Some comments, organizing and staging for next steps

--- src/heapsentryk/heapsentryk.c
+++ src/heapsentryk/heapsentryk.c
@@ -13,18 +13,26 @@ MODULE_LICENSE("GPL");
 
 void **sys_call_table;
 
+// This sets the bucket size for a process specific hashtable.
+// The size would be [1<<BUCKET_BITS_SIZE].
+// Example: Value of 5 implies that the size is 32.
 #define BUCKET_BITS_SIZE 5
-LIST_HEAD(process_list_head);
 
+// Define a linked list that holds information that connects PID with a process specific hashtable
+LIST_HEAD(pid_list);
+
+// The object that the link list holds.
+struct pid_entry {
+	int pid;
+	struct hlist_head *p_process_hashtable;
+	struct list_head pid_list_head;
+};
+
+// The object identified by a key(canary_location) in hashtable which holds canary information.
 struct canary_entry {
 	size_t canary_location;
 	size_t canary_value;
 	struct hlist_node next;
-};
-
-struct pid_entry {
-	struct hlist_head *p_process_hashtable;
-	struct list_head pid_list_head;
 };
 
 asmlinkage int (*original_fork) (void);
@@ -49,13 +57,27 @@ asmlinkage int heapsentryk_chmod(const char *pathname, int mode);
 
 void set_read_write(long unsigned int _addr);
 
-//asmlinkage struct canary_entry * find_canary_entry(struct hlist_head (*p_hashtable)[1 << BUCKET_BITS_SIZE]);
+asmlinkage struct canary_entry *find_canary_entry(struct
+						  hlist_head (*hashtable)[1 <<
+									  BUCKET_BITS_SIZE]);
 asmlinkage struct pid_entry *find_pid_entry(int pid);
+asmlinkage size_t sys_heapsentryk_canary(size_t not_used, size_t v2, size_t v3);
 
 asmlinkage struct pid_entry *find_pid_entry(int pid)
 {
-	printk("find_pid_entry entered\n");
-	return NULL;
+	struct list_head *position = NULL;
+	struct pid_entry *p_pid_entry = NULL;
+	struct pid_entry *entry = NULL;
+
+	list_for_each(position, &pid_list) {
+		p_pid_entry =
+		    list_entry(position, struct pid_entry, pid_list_head);
+		if (pid == p_pid_entry->pid) {
+			entry = p_pid_entry;
+			break;
+		}
+	}
+	return entry;
 }
 
 asmlinkage struct canary_entry *find_canary_entry(struct
@@ -64,7 +86,6 @@ asmlinkage struct canary_entry *find_canary_entry(struct
 {
 	int bucket_index = 0;
 	struct canary_entry *p_canary_entry = NULL;
-	printk("find_canary_entry entered\n");
 	hash_for_each((*hashtable), bucket_index, p_canary_entry, next) {
 		printk(KERN_INFO
 		       "canary_location=%d canary_value=%d deref=%d is in bucket %d\n",
@@ -73,11 +94,8 @@ asmlinkage struct canary_entry *find_canary_entry(struct
 		       *((size_t *) p_canary_entry->canary_location),
 		       bucket_index);
 	}
-	printk("Hashtable iteration ended\n");
 	return NULL;
 }
-
-asmlinkage size_t sys_heapsentryk_canary(size_t not_used, size_t v2, size_t v3);
 
 // System call which receives the canary information from heapsentryu
 // and stores it in its symbol table. This information is used by
@@ -89,18 +107,14 @@ asmlinkage size_t sys_heapsentryk_canary(size_t not_used, size_t v2, size_t v3)
 	size_t *p_group_count = (size_t *) v3;
 	size_t i = 0;
 
-	/*
 	struct pid_entry pid_list_entry;
-	struct list_head *position = NULL;
 	struct pid_entry *p_pid_entry = NULL;
-	*/
 
 	// Hashtable to store canary information
 	DEFINE_HASHTABLE(buckets, BUCKET_BITS_SIZE);
 	// Initialize the Hashtable to store the canary information.
 	hash_init(buckets);
 
-	printk("sys_heapsentryk_canary: buckets: %p\n", buckets);
 	printk
 	    ("heapsentryk:received: p_group_buffer:[%p] p_group_count:[%p] \n",
 	     p_group_buffer, p_group_count);
@@ -115,20 +129,47 @@ asmlinkage size_t sys_heapsentryk_canary(size_t not_used, size_t v2, size_t v3)
 		entry->canary_location = *(p_group_buffer + i * 2);
 		entry->canary_value = *((size_t *) * (p_group_buffer + i * 2));
 		hash_add(buckets, &entry->next, entry->canary_location);
-		printk("buf[%d][0]:[%p] buf[%d][1]:[%d] deref:[%d]\n", i,
-		       (void *)*(p_group_buffer + i * 2), i,
-		       *(p_group_buffer + i * 2 + 1),
-		       *((size_t *) * (p_group_buffer + i * 2)));
+		/*
+		   printk("buf[%d][0]:[%p] buf[%d][1]:[%d] deref:[%d]\n", i,
+		   (void *)*(p_group_buffer + i * 2), i,
+		   *(p_group_buffer + i * 2 + 1),
+		   *((size_t *) * (p_group_buffer + i * 2)));
+		 */
 	}
 
-	//find_pid_entry(10);
+	p_pid_entry = find_pid_entry(original_getpid());
+	if (!p_pid_entry) {
+		printk("pid_entry not found for pid:%d\n", original_getpid());
+		// TODO:
+		// 1) Create a new hashtable and add canary information into it.
+		// 2) Create and add a linked list entry for this process.
+	} else {
+		printk("Found pid_entry:%p not found for pid:%d\n", p_pid_entry,
+		       original_getpid());
+		// TODO:
+		// Add the canary information into existing hashtable.
+	}
+
+	// An object that connects PIDs to its corresponding hashtable.
+	pid_list_entry.pid = original_getpid();
+	pid_list_entry.p_process_hashtable = buckets;
+	INIT_LIST_HEAD(&pid_list_entry.pid_list_head);
+
+	// Adding the object 'pid_list_entry' into linked list by name 'pid_list'
+	list_add(&pid_list_entry.pid_list_head, &pid_list);
+
+	// Checking if there is an entry in the linked list corresponding to the current process.
+	p_pid_entry = find_pid_entry(original_getpid());
+	if (!p_pid_entry) {
+		printk("pid_entry not found for pid:%d\n", original_getpid());
+	} else {
+		printk("Found pid_entry:%p for pid:%d\n", p_pid_entry,
+		       original_getpid());
+	}
+
 	find_canary_entry(&buckets);
 
-	// TODO: Change the parameters to receive the canary information in bulk.
-	// This bulk count will be user configurable in the userspace.
-	// 
-	// Implement Hash tables or find out a good library which does our job easier.
-	return 30;
+	return 0;
 }
 
 asmlinkage int heapsentryk_execve(const char *filename, char *const argv[],
