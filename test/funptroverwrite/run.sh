843ea03c902b7beff3979995f726329445b71a68
Tabish Ahmad <tahmad@gitlab.com>
2015-11-19 00:51:32 -0500

    change syscal address in run.sh

--- test/funptroverwrite/run.sh
+++ test/funptroverwrite/run.sh
@@ -1,7 +1,7 @@
 #Disable ASLR
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 if [ "$1" == "protect" ]; then
-	MALICIOUS_INPUT=`perl -e 'printf "A" x 24 . "\x90\x31\xe5\xb7"'`
+	MALICIOUS_INPUT=`perl -e 'printf "A" x 24 . "\x90\x21\xe5\xb7"'`
 	export LD_PRELOAD=../../lib/libheapsentryu.so
 	./bookstore /bin/bash $MALICIOUS_INPUT
 elif [ "$1" == "attack" ]; then
